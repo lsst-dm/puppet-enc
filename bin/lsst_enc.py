4299a3758230e8457ff52d21ad6e451570338a36
Andres Villalobose <aevr007@gmail.com>
2018-11-22 13:28:06 -0300

    New approach to puppet enc
    Adding dynamic environment and dynamic names based on regular expression in the data base

--- bin/lsst_enc.py
+++ bin/lsst_enc.py
@@ -2,8 +2,8 @@
 # Python ENC
 # receives fqdn as argument
 
-#import sys
-#import re
+from datetime import datetime
+import re
 import yaml
 import sqlite3
 import argparse
@@ -25,6 +25,49 @@ def process_cmdline():
     return args
 
 
+# This will return the node definition that best matches 
+def find_node_definition(cursor, fqdn):
+    sql = "SELECT node_definition FROM Nodes"
+    node_regex = cursor.execute( sql )
+    selected_definition = None
+    aux = ""
+    for n in node_regex:
+        pattern = re.compile(n[0])
+        aux = re.search(pattern,fqdn)
+        if aux != None and len(str(aux)) > len(str(selected_definition)):
+            selected_definition = aux
+    if selected_definition == None:
+        return None
+    else:
+        return selected_definition.group(0)
+
+def find_temporary_environment(cursor, fqdn):
+    sql = "SELECT environment, ConfTimeStart, ConfTimeEnd FROM TemporaryEnvironmentConfiguration WHERE fqdn=?"
+    cursor.execute( sql, (fqdn,) )
+    r = cursor.fetchone()
+    rv = {}
+    if r is not None:
+        rv = { k: str(r[k]) for k in r.keys() }
+    
+    if len(rv) == 0:
+        return None
+    else:
+        conf_start = datetime.strptime(rv["ConfTimeStart"], '%Y-%m-%d %H:%M:%S')
+        conf_end = datetime.strptime(rv["ConfTimeEnd"], '%Y-%m-%d %H:%M:%S')
+
+        today = datetime.today()
+        if conf_end < today:
+            print("Delete this host from the environments database")
+            cursor.execute( "DELETE FROM TemporaryEnvironmentConfiguration WHERE fqdn=(?)", (fqdn,) )
+            return None
+        if conf_start > today:
+            print("This environment shouldn't be used yet")
+            return None
+        else:
+            print("This is the environment that should be used for this host during this period")
+            return rv["environment"]
+
+#TODO Improve the query in such a way that can be inferred from hiera_parameters.yaml
 def sql_lookup( dbfn, node ):
     """ def sql_lookup( dbfn, node ):
         Lookup fqdn in database and return hash of values for
@@ -36,13 +79,25 @@ def sql_lookup( dbfn, node ):
     conn = sqlite3.connect( dbfn )
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
-    sql = "select site,datacenter,cluster,role,environment from Nodes where node_definition=?"
-    c.execute( sql, (node,) )
-    r = c.fetchone()
-    rv = {}
-    if r is not None:
-        rv = { k: str(r[k]) for k in r.keys() }
-    return rv
+    node_definition = find_node_definition(c, node)
+    temp_env = find_temporary_environment(c, node)
+
+    if node_definition == None:
+        return {}
+    else:
+        sql = "select site,datacenter,cluster,role,environment from Nodes where node_definition = '" + node_definition + "'"
+        c.execute( sql )
+        r = c.fetchone()
+        rv = {}
+        if r is not None:
+            rv = { k: str(r[k]) for k in r.keys() }
+        
+        if temp_env != None:
+            rv["environment"] = temp_env
+
+        conn.commit()
+        c.close()
+        return rv
 
 
 def output_yaml(enc):
@@ -57,28 +112,28 @@ def run():
     args = process_cmdline()
 
     # basics
-    classes = [ "role::default" ]
+    classes = []
     parms = { "enc_hostname": args.fqdn }
 
     # Get info from DB
     db_data = sql_lookup( args.dbfilename, args.fqdn )
 
     # Attempt to extract environment from DB or use default value 'production'
-    env = db_data.pop( 'environment', 'production' )
+    env = db_data.pop( 'environment', 'develop' )
 
     # Fill in additional details from DB
     parms.update( db_data )
     try:
         role = db_data[ 'role' ]
     except ( KeyError ) as e:
-        classes.append( "hostname_problem" )
+        classes.append( "role::default" )
     else:
         classes = [ '{0}'.format( role ) ]
     
     # build hash for yaml
     enc = { 
         "classes": classes,
-        #"parameters": parms,
+        "parameters": parms,
         "environment": env,
     }
 
